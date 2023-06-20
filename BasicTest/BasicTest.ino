e04c5dea38c330afc72711195e775e43ddbbc402
Aria_RZA <ariarza1225@gmail.com>
2023-06-20 14:24:47 -0500

    Line sensor readings

--- BasicTest/BasicTest.ino
+++ BasicTest/BasicTest.ino
@@ -35,55 +35,21 @@ void selectTurtle(){
 }
 
 // Display Readings
-void loadCustomCharacters()
-{
-  static const char levels[] PROGMEM = {
-    0, 0, 0, 0, 0, 0, 0, 63, 63, 63, 63, 63, 63, 63
-  };
-  display.loadCustomCharacter(levels + 0, 0);  // 1 bar
-  display.loadCustomCharacter(levels + 1, 1);  // 2 bars
-  display.loadCustomCharacter(levels + 2, 2);  // 3 bars
-  display.loadCustomCharacter(levels + 3, 3);  // 4 bars
-  display.loadCustomCharacter(levels + 4, 4);  // 5 bars
-  display.loadCustomCharacter(levels + 5, 5);  // 6 bars
-  display.loadCustomCharacter(levels + 6, 6);  // 7 bars
-}
-void printBar(uint8_t height)
-{
-  if (height > 8) { height = 8; }
-  const char barChars[] = {' ', 0, 1, 2, 3, 4, 5, 6, (char)255};
-  display.print(barChars[height]);
-}
-void showReadings()
-{
-  display.clear();
-
-  while(!buttonB.getSingleDebouncedPress())
-  {
-    uint16_t position = lineSensors.readLineBlack(lineSensorVal);
-
+void showReadings(){
+  while(1){
+    display.clear();
+    lineSensors.read(lineSensorVal);
     display.gotoXY(0, 0);
-    display.print(position);
-    display.print("    ");
-    display.gotoXY(0, 1);
-    for (uint8_t i = 0; i < NUM_SENSOR; i++)
-    {
-      uint8_t barHeight = map(lineSensorVal[i], 0, 1000, 0, 8);
-      printBar(barHeight);
-    }
-
-    delay(50);
+    display.print(lineSensorVal[0]);
+    delay(100);
   }
 }
 
 void setup(){
-  loadCustomCharacters();
   selectTurtle();
-  motors.setSpeeds(maxSpeed, maxSpeed);
+  showReadings();
 }
 
 void loop(){
-  int16_t position = lineSensors.readLineBlack(lineSensorVal);
-  display.print(position);
   motors.setSpeeds(maxSpeed, maxSpeed);
 }
