f1e7b03793e864dc846a0701f99a67db001f7117
Joe reed <JoeReed2025@u.northwestern.edu>
2023-06-26 10:23:56 -0500

    Create Individual_Sensor_Test.ino

---
+++ Individual_Sensor_Test/Individual_Sensor_Test.ino
@@ -0,0 +1,136 @@
+# include <Pololu3piPlus32U4.h>
+# include <PololuMenu.h>
+
+# define NUM_SENSOR 5
+
+using namespace Pololu3piPlus32U4;
+
+OLED display; 
+
+LineSensors lineSensors;
+Motors motors;
+
+unsigned int lineSensorVal[NUM_SENSOR];
+
+uint16_t lineSpeed; // max is 400
+uint16_t calibSpeed; // max is 400
+uint16_t avepos;
+uint16_t sensorValues[5];
+uint16_t K = .5;
+uint16_t linePos;
+
+
+
+// Select Mode
+void selectHyper(){
+  lineSpeed = 300;
+  calibSpeed = lineSpeed;
+
+}
+void selectStandard(){
+  lineSpeed = 250;
+  calibSpeed = lineSpeed;
+}
+void selectTurtle(){
+  lineSpeed = 100;
+  calibSpeed = 60;
+}
+
+
+
+// Calibration
+void calibSensor(){
+  delay(1000);
+  display.clear();
+  display.print(F("Calib"));
+  for(uint16_t i = 0; i < 100; i++){
+    if (i > 25 && i <= 75){
+      motors.setSpeeds(calibSpeed,-calibSpeed);
+    }
+    else{
+      motors.setSpeeds(-calibSpeed,calibSpeed);
+    }
+    lineSensors.calibrate();
+  }
+  motors.setSpeeds(0,0);
+  display.clear();
+  display.print(F("Finished"));
+  delay(6000);
+}
+// Display Readings
+void showReading(){
+    for(uint16_t j = 0;j < 5; j++){
+        display.clear();
+        display.gotoXY(0, 0);
+        display.print(sensorValues[j]);
+        delay(1000);
+    }
+}
+
+
+
+
+void setup(){
+  selectTurtle();
+  calibSensor();
+  //showReading();
+}
+
+
+
+void loop(){
+  motors.setSpeeds(0,0);
+  lineSensors.read(sensorValues);
+  linePos = (int(sensorValues[2])-int(sensorValues[0])) + K*(int(sensorValues[2])-int(sensorValues[1])) + K*(int(sensorValues[3])-int(sensorValues[2])) + (int(sensorValues[4])-int(sensorValues[2]));
+  showReading();
+  display.clear();
+  display.print(F("Loop D"));
+  delay(1000);
+  display.clear();
+  display.print(int(linePos));
+  delay(2000);
+  
+}
+
+
+
+/*
+// Navigation
+void turnLeft(){
+  motors.setSpeeds(lineSpeed,lineSpeed * .85);
+  //display.print(F("TL"));
+  display.print(avepos);
+  delay(100);
+  justLeft = true;
+}
+void turnRight(){
+  motors.setSpeeds(lineSpeed * .85,lineSpeed);
+  //display.print(F("TR"));
+  display.print(avepos);
+  delay(100);
+  justRight = true;
+}
+void goStraight(){
+  
+  if (justRight){
+    turnLeft();
+    turnLeft();
+    justLeft = false;
+    justRight = false;
+  }
+  if (justLeft){
+    turnRight();
+    turnRight();
+    justRight = false;
+    justLeft = false;
+  }
+  
+  motors.setSpeeds(lineSpeed,lineSpeed);
+  //display.print(F("S"));
+  display.print(avepos);
+  delay(1000);
+}
+*/
+
+
+
