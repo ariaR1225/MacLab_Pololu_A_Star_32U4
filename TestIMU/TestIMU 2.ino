4342da2537ca3d743c255382fe13856e76ace202
Aria_RZA <ariarza1225@gmail.com>
2023-07-20 10:33:36 -0500

    update

---
+++ TestIMU/TestIMU 2.ino
@@ -0,0 +1,38 @@
+#include <Wire.h>
+#include <Pololu3piPlus32U4.h>
+#include <Pololu3piPlus32U4IMU.h>
+
+using namespace Pololu3piPlus32U4;
+
+IMU imu;
+
+//#include "TurnSensor.h"
+
+char buffer[100];
+
+void setup()
+{
+  Wire.begin();
+
+  if (!imu.init())
+  {
+    // Failed to detect the compass.
+    ledRed(1);
+    while(1)
+    {
+      Serial.println(F("Failed to initialize IMU sensors."));
+      delay(100);
+    }
+  }
+
+  imu.enableDefault();
+}
+void loop() {
+  imu.read();
+  snprintf_P(buffer, sizeof(buffer), PSTR("X: %6d Y: %6d Z: %6d"), 
+    imu.g.x, imu.g.y, imu.g.z
+  );
+  Serial.println(buffer);
+
+  delay(100);
+}
