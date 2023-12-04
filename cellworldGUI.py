13b8951a757118a9ce6d96f5070932624991cc72
Aria_RZA <ariarza1225@gmail.com>
2023-12-03 19:16:39 -0600

    GUI

--- cellworldGUI.py
+++ cellworldGUI.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
 
+
+### Add graphical feature and animation! 
+### Solve double click
+
 root = tk.Tk()
 root.title("Cell World GUI")
 # root.geometry("1000x200")
@@ -29,7 +33,57 @@ def feeder_test():
 def start_exp():
     pass
 
+def end_exp():
+    pass
 
+def upload_exp():
+    prefix = prefix_entry.get()
+    sub_name = subject_entry.get()
+    suffix = suffix_combobox.get()
+    check = input("This will LOCK terminal for several minutes. Proceed? (Y/N): ")
+    if check == "y" or check == "Y":
+        exp_name = prefix + sub_name + suffix
+        print(exp_name)
+        #     experiment_name = input("experiment_name" + "[" + str(defaults["experiment_name"]) + "]: ") or str(
+        #         defaults["experiment_name"])
+        #     experiment_join.join_episodes(experiment_name)
+        #     gdrive.upload_exp(experiment_name)
+        # continue
+    pass
+
+def reconnect():
+    # if command == "reconnect":
+    #     for key, client in clients.items():
+    #         try:
+    #             response = client.connect(ip[key])
+    #             if response:
+    #                 print(f'connected to {key}')
+    #             else:
+    #                 print(f'CANNOT connect to {key}!!!!')
+    #         except:
+    #             print(f'CANNOT connect to {key}!!!!')
+    pass
+
+"""
+def setting_update():
+    joint = joint_combobox.get()
+    MAF_enabled = maf_var.get()
+    window_size = int(win_scale.get())
+    PID_enabled = pid_var.get()
+    kp = float(kp_entry.get())
+    ki = float(ki_entry.get())
+    kd = float(kd_entry.get())
+    print("--------------------------")
+    print(joint)
+    if MAF_enabled:
+        print(f"MAF: window of size {window_size}")
+    else: 
+        print("MAF disabled")
+    if PID_enabled:
+        print(f"PID: kp = {kp}, ki = {ki}, kd = {kd}")
+    else:
+        print("PID disabled")
+"""
 # Creating Tkinter BooleanVar variables
 door0status = tk.BooleanVar()
 door1status = tk.BooleanVar()
@@ -98,15 +152,24 @@ prefix_label = ttk.Label(root,text="Prefix")
 prefix_label.grid(row=7,column=0,padx=10,pady=5)
 prefix_entry = ttk.Entry(root,textvariable=tk.StringVar,width=10)
 prefix_entry.grid(row=7,column=1,padx=10,pady=5)
+subject_label = ttk.Label(root,text="Subject")
+subject_label.grid(row=7,column=2,padx=10,pady=5)
+subject_entry = ttk.Entry(root,textvariable=tk.StringVar,width=20)
+subject_entry.grid(row=7,column=3,padx=10,pady=5)
 suffix_label = ttk.Label(root,text="Suffix")
-suffix_label.grid(row=7,column=2,padx=10,pady=5)
-suffix_combobox = ttk.Combobox(root,width=3,values=["RT"])
-suffix_combobox.grid(row=7,column=3,padx=10,pady=5)
+suffix_label.grid(row=7,column=4,padx=10,pady=5)
+suffix_combobox = ttk.Combobox(root,width=3,values=["RT","HT","CT"])
+suffix_combobox.grid(row=7,column=5,padx=10,pady=5)
 suffix_combobox.current(0)
 
 
 # Finish experiment
+exp_end_button = ttk.Button(root,text="Finish Experiment",command=end_exp)
+exp_end_button.grid(row=8,column=3,padx=10,pady=5)
 
+# Upload experiment
+exp_upload_button = ttk.Button(root,text="Upload Experiment",command=upload_exp)
+exp_upload_button.grid(row=8,column=4,padx=10,pady=5)
 
 # Resume experiment
 
@@ -119,10 +182,10 @@ suffix_combobox.current(0)
 # Connect
 
 # Reconnect
+connect_buttton = ttk.Button(root,text="(Re)connect",command=reconnect)
+connect_buttton.grid(row=12,column=0,padx=10,pady=5)
 
 # Refresh Connection
-
-# Upload experiment
 
 # Status
 
